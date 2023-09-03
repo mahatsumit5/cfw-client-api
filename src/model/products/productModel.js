2b7ea01b874aa590978ee566bd32b5a95c9563fe
mahat sumit <mahatsumit5@gmail.com>
2023-09-03 20:07:13 +1000

    get products by catagories finished

--- src/model/products/productModel.js
+++ src/model/products/productModel.js
@@ -1,6 +1,18 @@
 import mongoose from "mongoose";
 
+// const schema = new mongoose.Schema({ any: {} });
 const prouduct = mongoose.model("products", {});
+
 export const getProducts = () => {
   return prouduct.find();
+};
+export const getOneProduct = (_id) => {
+  return prouduct.findById(_id);
+};
+export const getProductsByCatagory = (filter) => {
+  const _id = new mongoose.Types.ObjectId(filter);
+  return prouduct.find({ parentCat: _id });
+};
+export const getSingleProduct = (filter) => {
+  return prouduct.findOne(filter);
 };
--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -1,17 +1,64 @@
 import express from "express";
-import { getProducts } from "../model/products/productModel.js";
+import {
+  getOneProduct,
+  getProducts,
+  getProductsByCatagory,
+  getSingleProduct,
+} from "../model/products/productModel.js";
 
 const router = express.Router();
-router.get("/", async (req, res) => {
+router.get("/:slug?/:_id?", async (req, res) => {
   try {
-    const data = await getProducts();
+    const { slug, _id } = req.params;
+
+    const data = slug
+      ? await getSingleProduct({ _id, slug })
+      : await getProducts();
+
+    if (slug && _id && !data) {
+      const result = await getProductsByCatagory(_id);
+      console.log(result, "------------------");
+      result?.length
+        ? res.json({
+            result,
+          })
+        : res.json({
+            status: "error no books found",
+            message: "No Products Found",
+          });
+      return;
+    }
     data
       ? res.json({
+          status: "success",
           data,
         })
-      : res.status(401).send("No Data Found");
+      : res.json({
+          status: "error",
+          message: `No product found with ${slug || "_id"}`,
+        });
   } catch (error) {
+    console.log(error);
     res.json({ error });
   }
 });
+// router.get("/catagories/:_id", async (req, res) => {
+//   try {
+//     const { _id } = req.params;
+//     console.log(_id);
+//     const result = await getProductsByCatagory(_id);
+//     console.log(result, "----s-------------------");
+//     result.length
+//       ? res.json({
+//           status: "success",
+//           result,
+//         })
+//       : res.json({
+//           status: "error",
+//           message: `No product found with ${slug || "_id"}`,
+//         });
+//   } catch (error) {
+//     res.json({ error });
+//   }
+// });
 export default router;
