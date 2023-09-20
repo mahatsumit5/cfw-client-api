bbfb3d9a3a2cd21baec55a62e539f6169bd4212e
mahat sumit <mahatsumit5@gmail.com>
2023-09-21 00:32:53 +1000

    stripe payment integrated

--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { insertOrder } from "../model/order/orderModel.js";
+import Stripe from "stripe";
 import { orderConfirmationEmail } from "../utils/nodeMailer.js";
+import e from "express";
 const router = express();
 router.post("/", async (req, res, next) => {
   try {
@@ -19,6 +21,22 @@ router.post("/", async (req, res, next) => {
     });
   } catch (error) {
     console.log(error);
+    next(error);
+  }
+});
+
+router.post("/checkout-with-stripe", async (req, res, next) => {
+  try {
+    const stripe = require("stripe")(
+      "sk_test_51NsR1aGbch8SC1C7pwtwxkjoWoWIjYlNBSxBWuCrhsP0tm5f4CFhEmzoLgg11L66nrDUDelrGpOPnwuAA9qBgt3100LNtzHHwo"
+    );
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: "https://example.com/success",
+      line_items: [{ price: "price_H5ggYwtDq4fbrJ", quantity: 2 }],
+      mode: "payment",
+    });
+  } catch (error) {
     next(error);
   }
 });
