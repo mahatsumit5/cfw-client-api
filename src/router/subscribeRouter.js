9e969e8d1ae19c3271adc4e542a6d6ca84c91286
mahat sumit <mahatsumit5@gmail.com>
2023-10-11 23:34:40 +1100

    .

--- src/router/subscribeRouter.js
+++ src/router/subscribeRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   try {
+    console.log(req.body);
     res.json({
       status: "success",
       message: "Thank you for subscribing.",
